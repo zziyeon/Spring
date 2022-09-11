7a9184820cd36615a51452592a8b3306f8d16063
zziyeon <jiyunsara99@naver.com>
2022-09-11 12:31:26 +0900

    add 'practice'file

---
+++ Spring/practive/src/main/java/com/kh/practive/PractiveApplication.java
@@ -0,0 +1,13 @@
+package com.kh.practive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractiveApplication.class, args);
+	}
+
+}
---
+++ Spring/practive/src/test/java/com/kh/practive/PractiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.practive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
