a6aa5721c345751e6fc9be17dcb83763e57a4cb2
zziyeon <jiyunsara99@naver.com>
2022-09-15 13:31:17 +0900

    수정중

---
+++ Spring/practice/src/main/java/com/kh/practive/domain/dao/ProductDAO.java
@@ -0,0 +1,47 @@
+package com.kh.practive.domain.dao;
+
+import com.kh.practive.domain.Product;
+
+import java.util.List;
+
+public interface ProductDAO {
+    /**
+     * 상품 등록
+     *
+     * @param product 상품 정보
+     * @return 등록된 상품 정보
+     */
+    Product save(Product product);
+
+    /**
+     * 상품조회
+     * @param productId 조회할 상품 아이디
+     * @return 상품 정보
+     */
+    Product findById(Long productId);
+
+    /**
+     * 상품 정보 수정
+     * @param productId 수정할 상품 아이디
+     * @param product 수정할 상품 정보
+     */
+    int update(Long productId, Product product);
+
+    /**
+     * 상품 삭제
+     * @param productId 삭제할 상품 아이디
+     */
+    int delete(Long productId);
+
+    /**
+     * 상품 목록
+     * @return 등록된 상품 목록
+     */
+    List<Product> findAll();
+
+    /**
+     * 상품 아이디 생성
+     * @return 생성된 상품 아이디
+     */
+    Long generatePid();
+}
---
+++ Spring/practice/src/main/java/com/kh/practive/web/form/product/EachInfoForm.java
@@ -0,0 +1,15 @@
+package com.kh.practive.web.form.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EachInfoForm {
+    private Long productId;     //상품아이디 PRODUCT_ID	NUMBER(10,0)
+    private String pname;       //상품명 PNAME	VARCHAR2(30 BYTE)
+    private Integer quantity;   //수량 QUANTITY	NUMBER(10,0)
+    private Integer price;      //가격 PRICE	NUMBER(10,0)
+}
